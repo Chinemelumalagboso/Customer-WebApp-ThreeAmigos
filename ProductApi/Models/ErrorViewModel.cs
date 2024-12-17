ad010fc8d537093594a206a07d70d1fa102ddda3
Chinemelum <158094669+Chinemelumalagboso@users.noreply.github.com>
2024-12-17 11:36:03 +0100

    update

--- ProductApi/Models/ErrorViewModel.cs
+++ ProductApi/Models/ErrorViewModel.cs
@@ -1,8 +1,8 @@
-namespace ProductApi.Models;
-
-public class ErrorViewModel
+namespace ProductApi.Models
 {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
